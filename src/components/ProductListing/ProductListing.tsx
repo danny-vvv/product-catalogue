8914a750c74c5383c7c5094c03eff24dcd851f81
Daniel Varcas <daniel.varcas@brighthr.com>
2023-08-15 17:43:17 +0100

    add brand image, was price, stock status, rating, styling to products.

--- src/components/ProductListing/ProductListing.tsx
+++ src/components/ProductListing/ProductListing.tsx
@@ -11,11 +11,33 @@ export function ProductListing({ product }: ProductListingProps) {
       <img src={product.image.url} alt={product.productName} />
 
       <div className="p-3">
+        <img
+          src={product.brand.brandImage.url}
+          alt={product.brand.name}
+          className="w-24"
+        />
         <h2 className="font-semibold">{product.productName}</h2>
-
         <p>
           {/* Would need to handle other currencies (product.price.currencyCode) */}
-          £{product.price.priceIncTax}
+          <span className="text-2xl font-bold text-red-600">
+            £{product.price.priceIncTax}
+          </span>
+
+          {product.price.wasPriceIncTax ? (
+            <s className="ml-2 text-gray-500">
+              Was {product.price.wasPriceIncTax}
+            </s>
+          ) : null}
+        </p>
+
+        {/* Would need to check product.stockStatus.status.
+        This is currently "G" for all products. 
+        Not sure what this means. "Good?" */}
+        <p>✅ In stock</p>
+
+        {/* Rating seems to be out of 5, representing stars, but would need to confirm this. */}
+        <p>
+          {product.averageRating} stars from {product.reviewsCount} reviews
         </p>
       </div>
     </div>
