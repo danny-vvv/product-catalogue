6ebeeb8f0e3e5585f5d19bdca4192dd0c2bd8ac7
Daniel Varcas <daniel.varcas@brighthr.com>
2023-08-15 19:17:44 +0100

    accept prices array in getListings. Change some types.

--- src/api/getListings.ts
+++ src/api/getListings.ts
@@ -1,15 +1,13 @@
 import axios, { AxiosResponse } from 'axios';
 import { Listings } from '../types/types';
 
-type Facets = {
-  prices: Array<{
-    identifier: string;
-    value: {
-      gte: number;
-      lte: number;
-    };
-  }>;
-};
+type Prices = {
+  // identifier: string; // Not sure how this works but doesn't appear to be necessary
+  value: {
+    gte: number;
+    lte: number;
+  };
+}[];
 
 type Payload = {
   query: string;
@@ -17,10 +15,18 @@ type Payload = {
   size: number;
   additionalPages: number;
   sort: number;
-  facets?: Partial<Facets>;
+  facets?: {
+    prices?: Prices;
+  };
 };
 
-export async function getListings({ sort = 1 }: { sort: number }) {
+export async function getListings({
+  sort = 1,
+  prices,
+}: {
+  sort: number;
+  prices?: Prices;
+}) {
   // sort:
   // 1: Recommended
   // 2: Lowest price
@@ -34,7 +40,11 @@ export async function getListings({ sort = 1 }: { sort: number }) {
       size: 0,
       additionalPages: 0,
       sort,
+      facets: {
+        prices,
+      },
     };
+
     const response: AxiosResponse<Listings> = await axios.post(
       'https://spanishinquisition.victorianplumbing.co.uk/interviews/listings?apikey=yj2bV48J40KsBpIMLvrZZ1j1KwxN4u3A83H8IBvI',
       payload
