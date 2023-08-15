f2572b4465b44ea6d0c92a998399f07adf8ab16e
Daniel Varcas <daniel.varcas@brighthr.com>
2023-08-15 18:30:20 +0100

    add sorting

--- src/api/getListings.ts
+++ src/api/getListings.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosResponse } from 'axios';
 import { Listings } from '../types/types';
 
-export async function getListings() {
+export async function getListings({ sort = 1 }: { sort: number }) {
   try {
     const response: AxiosResponse<Listings> = await axios.post(
       'https://spanishinquisition.victorianplumbing.co.uk/interviews/listings?apikey=yj2bV48J40KsBpIMLvrZZ1j1KwxN4u3A83H8IBvI',
@@ -10,7 +10,7 @@ export async function getListings() {
         pageNumber: 0,
         size: 0,
         additionalPages: 0,
-        sort: 1,
+        sort,
         // 1: Recommended (?)
         // 2: Lowest price
         // 3: Highest price
--- src/components/Listings.tsx
+++ src/components/Listings.tsx
@@ -1,23 +1,30 @@
 import React from 'react';
 import { Listings as IListings } from '../types/types';
-
 import { ProductGalleryView } from './ProductGalleryView/ProductGalleryView';
 import { getListings } from '../api/getListings';
 
 export function Listings() {
   const [listings, setListings] = React.useState<IListings | null>(null);
+  const [sort, setSort] = React.useState<number>(1);
 
   React.useEffect(() => {
     async function fetchData() {
-      const listings = await getListings();
+      const listings = await getListings({
+        sort,
+      });
+
+      // TODO: delete this when done:
+      console.log(listings);
+
       setListings(listings);
     }
 
     fetchData();
-  }, []);
+  }, [sort]);
 
-  // TODO: delete this when done:
-  console.log(listings);
+  function handleChangeSort(event: React.ChangeEvent<HTMLSelectElement>) {
+    setSort(Number(event.target.value));
+  }
 
   if (listings === null) {
     return <p>Loading...</p>;
@@ -30,11 +37,11 @@ export function Listings() {
           <label htmlFor="select-sort-method">
             Sort by
             <div>
-              <select id="select-sort-method">
-                <option>Recommended</option>
-                <option>Lowest price</option>
-                <option>Highest price</option>
-                <option>Highest discount</option>
+              <select id="select-sort-method" onChange={handleChangeSort}>
+                <option value="1">Recommended</option>
+                <option value="2">Lowest price</option>
+                <option value="3">Highest price</option>
+                <option value="4">Highest discount</option>
               </select>
             </div>
           </label>
